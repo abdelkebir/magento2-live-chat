35efd7934fbbc5b0660db72d251e5bc8029f3bc6
EL Harfali <elharfali.abdelkebir@gmail.com>
2018-09-04 23:10:59 +0100

    Structure improvements

---
+++ Block/Adminhtml/Dash.php
@@ -0,0 +1,35 @@
+<?php
+namespace Godogi\LiveChat\Block\Adminhtml;
+class Dash extends \Magento\Framework\View\Element\Template
+{
+	public $_storeManager;
+	public $_livechatFactory;
+
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context,
+								\Magento\Store\Model\StoreManagerInterface $storeManager,
+								\Godogi\LiveChat\Model\ResourceModel\Message\CollectionFactory $livechatFactory)
+	{
+		$this->_storeManager = $storeManager;
+		$this->_livechatFactory = $livechatFactory;
+		parent::__construct($context);
+	}
+
+	public function getSessions(){
+		$livechatCollection = $this->_livechatFactory->create();
+		$livechatCollection->setOrder('created_at','DESC');
+		$livechatCollection->getSelect()->group('session_id');
+		return $livechatCollection;
+	}
+	public function getDiscussions($sessionId){
+		$livechatCollection = $this->_livechatFactory->create();
+		$livechatCollection->setOrder('created_at','DESC');
+		$livechatCollection->addFieldToFilter('session_id', array('eq' => $sessionId));
+		return $livechatCollection;
+	}
+	public function getFormAdminAction(){
+		return $this->_storeManager->getStore()->getUrl('livechat/message/send');
+	}
+	
+	
+	
+}
